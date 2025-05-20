1d369c3f9f2f3e9b4f9eff6faf78e1f792f353d0
cxxz <xucongpsu@gmail.com>
2025-05-20 07:50:30 -0700

    update results for gemini-2.5-flash as user model

--- run_single_eval.sh
+++ run_single_eval.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-AGENT_MODEL="Qwen3-235B-A22B"
-AGENT_PROVIDER="hosted_vllm"
-USER_MODEL="gpt-4.1"
-USER_PROVIDER="azure"
+# AGENT_MODEL="Qwen3-235B-A22B"
+# AGENT_PROVIDER="hosted_vllm"
+
+AGENT_MODEL="gpt-4.1-mini"
+AGENT_PROVIDER="azure"
+USER_MODEL="gemini-2.5-flash-preview-04-17"
+USER_PROVIDER="gemini"
+
+# USER_MODEL="gpt-4.1"
+# USER_PROVIDER="azure"
 
 USER_STRATEGY="llm"
 ENV="retail"
 MAX_CONCURRENCY=8
-
-export AGENT_MODEL_THINK="false"
+x
+export DISABLE_USER_MODEL_THINK="true"
+# export DISABLE_AGENT_MODEL_THINK="true"
 export HOSTED_VLLM_API_BASE="http://xxx.yyy.zzz.net:8715/v1"
 
 export AZURE_API_KEY="your_key"
