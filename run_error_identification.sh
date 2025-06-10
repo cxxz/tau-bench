9a4a87baa95cc22aaa74a33e29096e4d0ee10e13
cxxz <xucongpsu@gmail.com>
2025-06-11 00:26:36 +0800

    update requirements.txt

--- run_error_identification.sh
+++ run_error_identification.sh
@@ -3,8 +3,10 @@
 # Interactive script to run error_identification.py
 
 # Prompt for input JSON file
-read -p "Enter input results JSON file [visualize_results/uploads/agent-gpt-4.1-mini_user-gemini-2.5-flash-preview-04-17-llm_range-0-10_0522050804.json]: " INPUT_JSON
-INPUT_JSON=${INPUT_JSON:-visualize_results/uploads/agent-gpt-4.1-mini_user-gemini-2.5-flash-preview-04-17-llm_range-0-10_0522050804.json}
+default_input="visualize_results/uploads/agent-gpt-4.1-mini_user-gemini-2.5-flash-preview-04-17-llm_range-0-10_0522050804.json"
+# default_input="results/retail-tool-calling/agent-o4-mini-low_user-gpt-4.1-llm_range-0-115_0527162356.json"
+read -p "Enter input results JSON file [${default_input}]: " INPUT_JSON
+INPUT_JSON=${INPUT_JSON:-$default_input}
 
 # Prompt for model name
 read -p "Enter model name [gpt-4.1-mini]: " MODEL
@@ -18,9 +20,9 @@ TASK_IDS=${TASK_IDS:-0}
 BASENAME=$(basename "$INPUT_JSON" .json)
 TASK_IDS_CLEAN=$(echo "$TASK_IDS" | tr ' ' '-')
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-OUTPUT_JSON="err_ana_${BASENAME}_tasks-${TASK_IDS_CLEAN}_judge-${MODEL}_${TIMESTAMP}.json"
+OUTPUT_JSON="err_ana_results/analysis_${BASENAME}_tasks-${TASK_IDS_CLEAN}_judge-${MODEL}_${TIMESTAMP}.json"
 
-export OPENAI_API_KEY="sk-pOImTYDnVtg1pSkv1Suusw"  # Set your OpenAI API key here
+export OPENAI_API_KEY="sk-yourapikey"  # Set your OpenAI API key here
 export OPENAI_BASE_URL="http://ai06.labs.hpecorp.net:4000"  # Set your OpenAI API base URL here
 
 set -e  # Exit on error
@@ -29,4 +31,4 @@ set -x # Enable debugging output
 # Run the script
 python error_identification.py --platform openai --model "$MODEL" -r "$INPUT_JSON" -o "$OUTPUT_JSON" -t $TASK_IDS
 
-echo "Output written to $OUTPUT_JSON"
+cat $OUTPUT_JSON
